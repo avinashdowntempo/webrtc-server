a9ad10de9f85586bbdd152585e64656fdd963b76
Avinash Jeyasankar <avinash.j@prodapt.com>
2018-10-04 16:31:11 +0530

    latest socket

--- app.js
+++ app.js
@@ -13,17 +13,37 @@ app.io = io;
 var users = [];
 
 io.on("connection", function (socket) {
+    socket.on('disconnect', function () {
+        delete users[socket.user];
+        console.log(users);
+    });
     console.log("A user connected");
     socket.on('login', function (data) {
         console.log('connected user' + socket.id + ' ' + data.user);
-        users.push(data.user);
+        users[data.user] = socket.id;
+        console.log(users);
         socket.user = data.user;
     });
     socket.on('connect-user', function (data) {
         console.log('connect to other user' + socket.id + ' ' + data.user);
     });
     socket.on('send-ice', function (data) {
-        console.log('ice from' + socket.id + ' ' + data.ice);
+        console.log('ice' + socket.id + ' ' + data + 'remote user' + JSON.stringify(data.to));
+        console.log(data.ice);
+        console.log('to user socket' + users[data.to]);
+        io.to(users[data.to]).emit('receive-ice', data.ice);
+    });
+    socket.on('send-offer', function (data) {
+        console.log('ice' + socket.id + ' ' + data + 'remote user' + JSON.stringify(data.to));
+        console.log(data.offer);
+        console.log('to user socket' + users[data.to]);
+        io.to(users[data.to]).emit('receive-offer', data.offer);
+    });
+    socket.on('send-answer', function (data) {
+        console.log('ice' + socket.id + ' ' + data + 'remote user' + JSON.stringify(data.to));
+        console.log(data.answer);
+        console.log('to user socket' + users[data.to]);
+        io.to(users[data.to]).emit('receive-answer', data.answer);
     });
 });
 
